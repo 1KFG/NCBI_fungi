f724728382b73758d304177bff1d8ea8ba58d439
Jason Stajich <jasonstajich.phd@gmail.com>
2021-02-21 23:29:22 -0800

    moved folders

--- scripts/get_ncbi_daatasets.sh
+++ scripts/get_ncbi_daatasets.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/
 
-if [ ! -f datasets ]; then
-	curl -O https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/dataformat
-	curl -O https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets
+BINDIR=bin
+mkdir -p $BINDIR
+
+if [ ! -f $BINDIR/datasets ]; then
+	curl -o $BINDIR/dataformat https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/dataformat
+	curl -o $BINDIR/datasets https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/LATEST/linux-amd64/datasets
 
 	chmod +x dataformat datasets
 fi
-ACCESSION=ncbi_accessions.json
+ACCESSION=lib/ncbi_accessions.json
+
 if [ ! -s ]; then
-	./datasets summary genome taxon fungi > $ACCESSION
+	$BINDIR/datasets summary genome taxon fungi > $ACCESSION
 fi
