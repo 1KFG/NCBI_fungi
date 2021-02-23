1067acbfd587aae5e8ad5fe1eb236654a6407cc9
Jason Stajich <jasonstajich.phd@gmail.com>
2021-02-22 23:32:51 -0800

    typo in name

--- pipeline/00_init_parse.sh
+++ pipeline/00_init_parse.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 #SBATCH -p short
 
-bash scripts/get_ncbi_daatasets.sh
+bash scripts/get_ncbi_datasets.sh
 ./scripts/assembly_json_process.py --infile lib/ncbi_accessions.json --outfile lib/ncbi_accessions.csv
