289c75fea0d15fafeb0f84301ded09e9bfe7c255
Jason Stajich <jasonstajich.phd@gmail.com>
2021-02-24 09:38:08 -0800

    add download

--- pipeline/00_init_parse.sh
+++ pipeline/00_init_parse.sh
@@ -3,3 +3,5 @@
 
 bash scripts/get_ncbi_datasets.sh
 ./scripts/assembly_json_process.py --infile lib/ncbi_accessions.json --outfile lib/ncbi_accessions.csv
+
+perl scripts/make_taxonomy_table.pl lib/ncbi_accessions.csv > lib/ncbi_accessions_taxonomy.csv
