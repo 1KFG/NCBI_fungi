fb9f2556dd95d438ca4c53c31a796cacb309e497
Jason Stajich <jasonstajich.phd@gmail.com>
2022-11-03 12:46:05 -0700

    add dir creation

--- scripts/get_taxonkit.sh
+++ scripts/get_taxonkit.sh
@@ -8,8 +8,8 @@ if [ ! -f $BINDIR/taxonkit ]; then
 	curl -L https://github.com/shenwei356/taxonkit/releases/download/v0.13.0/taxonkit_linux_amd64.tar.gz | tar zxf - -C $BINDIR
 fi
 
-if [ ! -f $TAXONDIR ]; then
-	mkdir $TAXONDIR
+if [ ! -d $TAXONDIR ]; then
+	mkdir -p $TAXONDIR
 fi
 if [ ! -f $TAXONDIR/node.dmp ]; then
 	curl -L ftp://ftp.ncbi.nih.gov:/pub/taxonomy/taxdump.tar.gz | tar zxf - -C  $TAXONDIR
