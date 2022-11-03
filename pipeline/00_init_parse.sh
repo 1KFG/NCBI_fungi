fb83cf287e6ead7ef312a6f9b5ebfe2cd3f43813
Jason Stajich <jasonstajich.phd@gmail.com>
2022-11-03 14:48:53 -0700

    use new strategy for taxonkit

--- pipeline/00_init_parse.sh
+++ pipeline/00_init_parse.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash -l
-#SBATCH -p short --mem 24gb -C xeon 
+#SBATCH -p short --mem 24gb -C xeon
 
 bash scripts/get_ncbi_datasets.sh
+bash scripts/get_taxonkit.sh
 ./scripts/assembly_json_process.py --infile lib/ncbi_accessions.json --outfile lib/ncbi_accessions.csv
-
-module load bioperl
-perl scripts/make_taxonomy_table.pl lib/ncbi_accessions.csv > lib/ncbi_accessions_taxonomy.csv
+./scripts/add_taxonomy.py --infile lib/ncbi_accessions.csv --outfile lib/ncbi_accessions_taxonomy.csv
