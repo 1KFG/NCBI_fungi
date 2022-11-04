664433d63ac89670a2447f55d329a986221ede1f
Jason Stajich <jasonstajich.phd@gmail.com>
2022-11-03 20:48:07 -0700

    support multi thread

--- pipeline/00_init_parse.sh
+++ pipeline/00_init_parse.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash -l
-#SBATCH -p short --mem 24gb -C xeon --out logs/init_parse.log
+#SBATCH -p short -N 1 -n 1 -c 8 --mem 24gb -C xeon --out logs/init_parse.log
 
+CPU=8
 bash scripts/get_ncbi_datasets.sh
 bash scripts/get_taxonkit.sh
 ./scripts/assembly_json_process.py --infile lib/ncbi_accessions.json --outfile lib/ncbi_accessions.csv
-./scripts/add_taxonomy.py --infile lib/ncbi_accessions.csv --outfile lib/ncbi_accessions_taxonomy.csv
+echo "setting up taxonomy lookups"
+./scripts/add_taxonomy.py --infile lib/ncbi_accessions.csv --outfile lib/ncbi_accessions_taxonomy.csv --cpu $CPU
