10069bc88c2d383c6220c4d2223f9bf649885930
Jason Stajich <jasonstajich.phd@gmail.com>
2024-08-16 14:35:58 -0700

    run in parallel for speed

--- pipeline/01b_make_genomefolder.sh
+++ pipeline/01b_make_genomefolder.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash -l
-pushd Public_genomes
-./scripts/create_genome_files.py --all
+#SBATCH --mem 24gb -p short -c 96 -n 1 -N 1
+CPU=2
+if [ ! -z $SLURM_CPUS_ON_NODE ]; then
+    CPU=$SLURM_CPUS_ON_NODE
+fi
+#pushd Public_genomes
+MAX=$(expr `wc -l lib/ncbi_accessions.csv | awk '{print $1}'` - 1)
+parallel -j $CPU ./scripts/create_genome_files.py --index {} ::: $(seq $MAX)
